32e2e37f711f89f5d2064d8330ef3a61065d3253
Smith <Cameron.Smith@dot.ny.gov>
2018-05-29 10:34:16 -0400

    Functioning timer in elm.

---
+++ timer.elm
@@ -0,0 +1,83 @@
+import Html exposing ( Html, div, text, beginnerProgram, button )
+import Html.Attributes exposing ( style )
+import Html.Events exposing ( onClick )
+import Time exposing ( Time, second )
+
+main = Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
+
+-- Models
+
+type alias Model = 
+  { time : Time
+  , ticking : Bool  
+  }
+
+init : (Model, Cmd Msg)
+init =
+  (Model 0 False, Cmd.none)
+
+
+-- Update
+
+timeIncrement : Time
+timeIncrement = Time.second
+
+type Msg = Tick Time
+         | Start
+         | Stop
+         | Reset
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = 
+  case msg of 
+    Tick newTime ->
+      ( Model (timeIncrement + model.time) True, Cmd.none )
+    Start ->
+      ( Model model.time True, Cmd.none )
+    Stop ->
+      ( Model model.time False, Cmd.none )
+    Reset ->
+      ( Model 0 False, Cmd.none )
+
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model = 
+  if model.ticking == True then
+    Time.every timeIncrement Tick
+  else
+    Sub.none
+
+
+-- View
+
+view : Model -> Html Msg
+view model =
+  div [ centeredLayout ] 
+    [ text (toString (Time.inSeconds model.time)) 
+    ,  div [] 
+      [ button [ onClick Start ] [ text "Start" ]
+      , button [ onClick Stop ] [ text "Stop" ]
+      , button [ onClick Reset ] [ text "Reset" ]
+      ]
+
+    ]
+  
+
+-- Styles
+
+centeredLayout : Html.Attribute msg
+centeredLayout = 
+  style
+    [ ("display", "flex")
+    , ("flex-direction", "column")
+    , ("justifyContent", "center")
+    , ("alignItems", "center")
+    , ("height", "100%")
+    ]
+
+
+
+
+
